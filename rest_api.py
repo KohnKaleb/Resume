f39c09e632f1416f049601c81715dc0cf02a73e1
kalebkohn <kjkohn@wisc.edu>
2023-09-29 13:16:13 -0500

    got api fully functional, provided documentation, and answered summary questions

--- rest_api.py
+++ rest_api.py
@@ -8,6 +8,18 @@ api = Api(app)
 stored_transactions = []
 
 class add(Resource):
+    """
+    adds a valid transaction to the stored_transactions list
+
+    args: JSON in format
+        {
+            "payer": "company_name",
+            "points": point_value,
+            "timestamp": "timestamp_date"
+        }
+
+    returns: 200 if successful, 400 if not
+    """
     def post(self):
        added_points = request.get_json()
        stored_transactions.append(added_points)
@@ -18,10 +30,20 @@ class add(Resource):
        return '', 200
     
 class spend(Resource):
+    """
+    updates the stored_transactions list to reflect the spent points
+
+    args: JSON in format
+        {
+            "points": point_value
+        }
+
+    returns: 200 if successful and the amount of points that each payer spend on the transaction, 400 if not
+    """
     def post(self):
         spent_points = request.get_json()
-        stored_transactions.append(spent_points)
         result = []
+        result_dict = {}
 
         if "points" not in spent_points:
             return '', 400
@@ -29,28 +51,45 @@ class spend(Resource):
         sorted_transactions = sorted(stored_transactions, key=lambda k : k["timestamp"])
         points = spent_points["points"]
         for transaction in sorted_transactions:
-            if transaction["points"] <= points:
+            if transaction["points"] < points:
                 points -= transaction["points"]
                 transaction["points"] = 0
-                result.append({transaction["payer"]: -transaction["points"]})
+                if transaction["payer"] not in result_dict:
+                    result_dict[transaction["payer"]] = 0
+                result_dict[transaction["payer"]] += -transaction["points"]
             else:
                 transaction["points"] -= points
-                result.append({transaction["payer"]: -points})
+                if transaction["payer"] not in result_dict:
+                    result_dict[transaction["payer"]] = 0
+                result_dict[transaction["payer"]] += -points
                 points = 0
         
         if points != 0:
             return 'Not Enough Points!', 400
 
-        return jsonify(result), 200
+        result.append(result_dict)
+
+        return result, 200
 
 class balance(Resource):
+    """
+    returns the current balance of each payer
+
+    returns: 200 if successful and the current balance of each payer
+    """
     def get(self):
         result = {}
+
         for transaction in stored_transactions:
-            if transaction["payer"] not in result:
-                result[transaction["payer"]] = 0
-            result[transaction["payer"]] += transaction["points"]
-        return jsonify(result), 200      
+            if "payer" in transaction and "points" in transaction:
+                payer = transaction["payer"]
+                points = transaction["points"]
+                
+                if payer not in result:
+                    result[payer] = 0
+                result[payer] += points
+
+        return result, 200
 
 api.add_resource(add, '/add')
 api.add_resource(spend, '/spend')
